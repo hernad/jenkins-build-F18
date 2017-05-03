3a50f61ecbd46493573a2d1cf39e801acc3c3133
hernad <hernad@bring.out.ba>
2017-05-03 10:06:50 +0200

    data F18 core vdi

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VDI_BASE=~/data_harbour_core_linux.vdi 
+VDI_BASE=~/data_F18_core_linux.vdi 
 
 [ ! -f $VDI_BASE ]  || VBoxManage createmedium --size 20000 --format VDI --filename $VDI_BASE
 
