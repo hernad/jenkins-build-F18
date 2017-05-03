7e1c0dacbfe71c43334570fc875ea651bbcbb453
hernad <hernad@bring.out.ba>
2017-05-03 10:08:58 +0200

    fix

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,7 +2,7 @@
 
 VDI_BASE=~/data_F18_core_linux.vdi 
 
-[ ! -f $VDI_BASE ]  || VBoxManage createmedium --size 20000 --format VDI --filename $VDI_BASE
+[ -f $VDI_BASE ]  || VBoxManage createmedium --size 20000 --format VDI --filename $VDI_BASE
 
 [ -f data.vdi ] || VBoxManage clonemedium $VDI_BASE data.vdi
 
