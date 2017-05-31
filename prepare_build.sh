fbfd31881964a09457d6694f5ebd4f5647b52cc1
hernad <hernad@bring.out.ba>
2017-05-31 12:05:40 +0200

    delete old VM HDD

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,6 +2,22 @@
 
 VDI_BASE=~/data_F18_core_linux.vdi 
 
+if [ ! -f data.vdi ] ; then
+
+  VM=`VBoxManage list vms | grep ^\"F18-linux-i386_default_ | tail -1 | awk '{print $2}'`
+  if [ -n "$VM" ] ; then
+    echo "erasing old VM $VM"
+    VBoxManage unregistervm $VM --delete
+  fi
+
+  HDD=`VBoxManage list hdds -l | grep "Location.*workspace/F18-linux-i386/data.vdi"  -B5 | grep "^UUID:" | awk '{print $2}'`
+  if [ -n "$HDD" ] ; then
+     echo "erasing old HDD"
+     VBoxManage closemedium $HDD --delete
+  fi
+
+fi
+
 [ -f $VDI_BASE ]  || VBoxManage createmedium --size 20000 --format VDI --filename $VDI_BASE
 
 [ -f data.vdi ] || VBoxManage clonemedium $VDI_BASE data.vdi
