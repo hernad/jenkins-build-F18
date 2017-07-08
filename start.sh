5d55adcc3dff7bcc0dd189f6a631be891fa8222c
hernad <hernad@bring.out.ba>
2017-07-08 21:24:25 +0200

    F18 linux font

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+F18_FONT=xfonts-terminus-oblique
+
+if ! dpkg -l $F18_FONT
+   apt-get install -y $F18_FONT
+fi
+ 
 sudo mount -t vboxsf -o uid=$UID,gid=$(id -g) vagrant /vagrant
 
 if ! cat /proc/mounts | grep /data ; then
