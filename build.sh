485a8d41dab18d0b9b2fdda192393659394188c9
hernad <hernad@bring.out.ba>
2017-07-20 15:55:46 +0200

    fix F18_VER, F18_VER_E, F18_VER_X

--- build.sh
+++ build.sh
@@ -63,23 +63,24 @@ export F18_POS=1
 export F18_RNAL=1
 
 F18_VER=`cat VERSION`
+cp VERSION /vagrant/F18_VER
 if [ -n "$F18_VER" ] && [ -f VERSION ] && [ ! -f /vagrant/F18_Ubuntu_i686_${F18_VER}.gz ] ; then
    git checkout -f $F18_VER
    [ $? -ne 0 ] && echo "git checkout $F18_VER ERROR" && exit 1
    scripts/build_gz_push.sh Ubuntu_i686 ${F18_VER}
-   cp VERSION /vagrant/F18_VER
    cp F18_Ubuntu_i686_${F18_VER}.gz /vagrant/
 else
+   cp VERSION /vagrant/F18_VER
    echo "F18_VER ${F18_VER} already exists"
 fi
 
 git checkout $BRANCH -f
 F18_VER_E=`cat VERSION_E`
+cp VERSION_E /vagrant/F18_VER_E
 if [ -n "$F18_VER_E" ] && [ -f VERSION_E ] &&  [ ! -f /vagrant/F18_Ubuntu_i686_${F18_VER_E}.gz ] ; then
    git checkout -f $F18_VER_E
    [ $? -ne 0 ] && echo "git checkout $F18_VER_E ERROR" && exit 1
    scripts/build_gz_push.sh Ubuntu_i686 ${F18_VER_E}
-   cp VERSION_E /vagrant/F18_VER_E
    cp F18_Ubuntu_i686_${F18_VER_E}.gz /vagrant/
 else
    echo "F18_VER_E ${F18_VER_E} already exists"
@@ -87,11 +88,11 @@ fi
 
 git checkout $BRANCH -f
 F18_VER_X=`cat VERSION_X`
+cp VERSION_X /vagrant/F18_VER_X
 if [ -n "$F18_VER_X" ] &&  [ -f VERSION_X ] &&  [ ! -f /vagrant/F18_Ubuntu_i686_${F18_VER_X}.gz ] ; then
    git checkout -f $F18_VER_X
    [ $? -ne 0 ] && echo "git checkout $F18_VER_X ERROR" && exit 1
    scripts/build_gz_push.sh Ubuntu_i686 ${F18_VER_X}
-   cp VERSION_X /vagrant/F18_VER_X
    cp F18_Ubuntu_i686_${F18_VER_X}.gz /vagrant/
 else
    echo "F18_VER_X ${F18_VER_X} already exists"
