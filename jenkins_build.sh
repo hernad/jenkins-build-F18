4f04cea4b337f6469f152f106eaaa778005c9511
hernad <hernad@bring.out.ba>
2017-07-20 16:16:39 +0200

    fix F18_VER, F18_VER_E, F18_VER_X

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -13,7 +13,7 @@ fi
 
 SSH_DOWNLOAD_SERVER=docker@192.168.168.171
 
-DOWNLOADS_DIR=/data_0/f18-downloads_0/downloads.bring.out.ba/www/files/
+DOWNLOADS_DIR=/data_0/f18-downloads_0/downloads.bring.out.ba/www/files
 
 
 if [ ! -f data.vdi ] ; then
