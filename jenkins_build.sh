3c94096372b361de205f364a1ff44f3ab2755c43
Ernad Husremović <hernad@bring.out.ba>
2018-01-02 14:52:56 +0100

    Update jenkins_build.sh

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -11,7 +11,7 @@ else
    echo 23100-ld > F18_BRANCH
 fi
 
-SSH_DOWNLOAD_SERVER=docker@192.168.168.171
+SSH_DOWNLOAD_SERVER=docker@192.168.168.181
 
 DOWNLOADS_DIR=/data_0/f18-downloads_0/downloads.bring.out.ba/www/files
 
